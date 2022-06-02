5d197fdcaaf2c162ff40a130e109a2febe8669d2
KatherineCaley <u6096186@anu.edu.au>
2022-06-02 16:53:38 +1000

    MAINT: black

--- src/colour_norm/luminosity.py
+++ src/colour_norm/luminosity.py
@@ -2,16 +2,17 @@ import cv2
 
 import numpy as np
 
+
 def luminosity_percentile(img, pctl: int):
 
-    img = cv2.imread(img,1)
-    rows,cols, _ = img.shape
+    img = cv2.imread(img, 1)
+    rows, cols, _ = img.shape
 
     luminosities = []
 
     for i in range(rows):
         for j in range(cols):
-            R, G, B = img[i,j]
-            luminosities.append(sum([R, G, B])/3)
+            R, G, B = img[i, j]
+            luminosities.append(sum([R, G, B]) / 3)
 
     return np.percentile(luminosities, pctl)
--- tests/test_luminosity.py
+++ tests/test_luminosity.py
@@ -1,7 +1,10 @@
-
 from colour_norm.luminosity import luminosity_percentile
 
+
 def test_luminosity_percentile():
-    lum = luminosity_percentile("/Users/katherine/repos/ColourNormalisation/tests/data/images_raw/00027/reference.jpg", 95)
+    lum = luminosity_percentile(
+        "/Users/katherine/repos/ColourNormalisation/tests/data/images_raw/00027/reference.jpg",
+        95,
+    )
 
     print(lum)
